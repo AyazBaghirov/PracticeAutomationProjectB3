81610adf8833b4e6a5bd8e66169bbd16b2dc37aa
Admin <ayazbagirov89@gmail.com>
2024-06-17 18:43:12 +0400

    My first Commit

--- src/test/java/io/loop/test/Task1.java
+++ src/test/java/io/loop/test/Task1.java
@@ -3,5 +3,6 @@ package io.loop.test;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
